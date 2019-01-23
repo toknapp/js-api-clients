7a1395403f9e12afed30280e2395defec3a182af
Frederik Krautwald <fkrautwald@gmail.com>
2019-01-23 15:20:59 +0100

    Add cli-wallet tutorial configuration
    This commit adds configuration for the cli-wallet tutorial.
    
    Signed-off-by: Frederik Krautwald <fkrautwald@gmail.com>

---
+++ tutorials/01-cli-wallet/config.js
@@ -0,0 +1,17 @@
+/*!
+ * Copyright © 2018-present Upvest GmbH. All rights reserved.
+ *
+ * License is found in the LICENSE file in the root directory of this source tree.
+ */
+
+export const BASE_URL = "https://api.eu.upvest.co/1.0";
+
+/* The tenancy API key.
+ * The default provided details will use the Upvest playground, which accesses
+ * protocol testnets. Replace with your own tenancy API key details.
+ */
+export const TENANCY_API_KEY = {
+  key: "ItPd0Zp6LuoWtF8gwQbr-g",
+  secret: "XFWYYrCpeP3RkWzK5_yDL18JH3paE6WBQ6Qe4UJWG80",
+  passphrase: "eomvRYUV1SGyoyk4GeOBA5dCx-9cznqyGR6rwowagqI"
+};
