588a86a52fb39fa811602c381fc0416fbf1706d5
Raphael Huefner <raphael@upvest.co>
2019-02-25 11:39:10 +0100

    Add separate script to delete all test users after a test run

--- packages/api-tests/cli-options.js
+++ packages/api-tests/cli-options.js
@@ -3,6 +3,7 @@ const program = require('commander');
 
 program
   .option('-c, --config [type]', 'Use the specified configuration file [.test_config.json]', '.test_config.json')
+  .option('-f, --force', 'Force something')
   .parse(process.argv);
 
 const configFilePath = resolve(program.config);
@@ -11,4 +12,4 @@ console.log('Using the "%s" config file', configFilePath);
 
 test_config = require(configFilePath);
 
-module.exports = { configFilePath, test_config };
+module.exports = { configFilePath, test_config, forced:Boolean(program.force) };
---
+++ packages/api-tests/delete-all-users.js
@@ -0,0 +1,56 @@
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+
+const xmlParser = require('fast-xml-parser');
+
+const cryptoRandomString = require('crypto-random-string');
+
+const { UpvestTenancyAPI } = require('@upvest/tenancy-api');
+const { UpvestClienteleAPI } = require('@upvest/clientele-api');
+
+const {
+  inspect, inspectError, tGetCachedOrCreateUser, tCreateUser, tEcho,
+  tCreateWallets, tWaitForWalletActivation, readlineQuestionPromise,
+} = require('./util.js');
+
+const { test_config, forced } = require('./cli-options.js');
+
+const tenancy = new UpvestTenancyAPI(
+  test_config.baseURL,
+  test_config.first_apikey.key,
+  test_config.first_apikey.secret,
+  test_config.first_apikey.passphrase_last_chance_to_see,
+);
+
+const getClienteleAPI = (username, password) => new UpvestClienteleAPI(
+  test_config.baseURL,
+  test_config.first_oauth2_client.client_id,
+  test_config.first_oauth2_client.client_secret,
+  username,
+  password
+);
+
+async function deleteAllUsers() {
+  if (! forced) {
+    inspect('Please use the --force command line switch to delete all users.');
+    return;
+  }
+  for await (const user of tenancy.users.list()) {
+    if (username.startsWith('txtest-')) {
+      // Skip users for which funds might have gotten stuck in a failed transaction.
+      continue;
+    }
+
+    let isDeleted;
+    try {
+      isDeleted = await tenancy.users.delete(username);
+    }
+    catch (error) {
+      inspect('Deleting the user failed.');
+      inspectError(error);
+    }
+    inspect('The user was deleted:', isDeleted);
+  }
+}
+
+deleteAllUsers();
