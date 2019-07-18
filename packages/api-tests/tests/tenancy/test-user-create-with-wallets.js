d321ae245fceedefb3c5264b91bf1abdbf874f9e
Raphael Huefner <raphael@upvest.co>
2019-07-18 12:36:44 +0200

    Make assets list in 1 test configurable

--- packages/api-tests/tests/tenancy/test-user-create-with-wallets.js
+++ packages/api-tests/tests/tenancy/test-user-create-with-wallets.js
@@ -8,15 +8,11 @@ const { test, inspect } = testenv;
 test('Testing users.create() with wallet creation', async function (t) {
   const clientIp = '127.0.0.1';
   const userAgent = 'Upvest JS API client test script';
-  const assetIds = [
-    testenv.config.assetIds.Arweave,
-    testenv.config.assetIds.Bitcoin,
-    testenv.config.assetIds.Ether,
-    testenv.config.assetIds.ExampleERC20,
-  ];
+  const assetIds = Object.values(testenv.config.assetIds);
   const { username, password, recoverykit, wallet_ids } = await partials.tCreateUser(t, testenv.tenancy, clientIp, userAgent, assetIds);
   if (! username) return;
 
+  // TODO Make number of "merging" assets configurable.
   t.equal(wallet_ids.length, assetIds.length - 1, 'Number of wallets created is one less than number of requested assets because ETH and ERC20 get combined in 1 wallet.');
 
   partials.tIsRecoveryKitValid(t, recoverykit);
