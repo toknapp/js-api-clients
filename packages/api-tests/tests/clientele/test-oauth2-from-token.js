5df9de4b60943ccdaf84abd5309c299e09a9fb4e
Yao Adzaku <yao.adzaku@gmail.com>
2020-03-31 11:15:10 -0400

    fix reference issues

--- packages/api-tests/tests/clientele/test-oauth2-from-token.js
+++ packages/api-tests/tests/clientele/test-oauth2-from-token.js
@@ -2,27 +2,25 @@ const testenv = require('../../testenv.js');
 const partials = require('../../partials.js');
 
 // Shortcuts to most-used facilities.
-const { test, inspect } = testenv;
+const {test, inspect} = testenv;
 
-
-test('Testing OAuth2 token re-use', async function (t) {
-  const { username, password } = await partials.tCreateUser(t, testenv.tenancy);
-  if (! username) return;
+test('Testing OAuth2 token re-use', async function(t) {
+  const {username, password} = await partials.tCreateUser(t, testenv.tenancy);
+  if (!username) return;
   const clientele = testenv.getClienteleAPI(username, password);
   t.comment('Test echo with "normal" Clientele API wrapper');
   const echoSuccess = await partials.tEcho(t, clientele);
-  if (! echoSuccess) return;
+  if (!echoSuccess) return;
 
   t.comment('Test echo with a Clientele API wrapper which allows to re-use an OAuth2 token');
   const oauth2Token = await clientele.getCachedToken();
 
   tokenClientele = new testenv.UpvestClienteleAPIFromOAuth2Token(
-    testenv.config.baseURL,
-    oauth2Token
+    testenv.ClienteleOAuthConfig(testenv.config.baseURL, oauth2Token)
   );
 
   const tokenEchoSuccess = await partials.tEcho(t, tokenClientele);
-  if (! tokenEchoSuccess) return;
+  if (!tokenEchoSuccess) return;
 
   t.end();
 });
